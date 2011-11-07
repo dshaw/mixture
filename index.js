c994fbb85d70ef6629258bed4e5c814f24e652c1
Daniel Shaw <github@dshaw.com>
2011-11-06 17:11:10 -0800

    Hello, Mixture.

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * mixture
+ * Copyright(c) 2011 Daniel D. Shaw <dshaw@dshaw.com>
+ * MIT Licensed
+ */
+
+/**
+ * Mixture
+ */
+
+exports = module.exports = require('./lib/mixture.js');
+
+/**
+ * Version
+ */
+
+exports.version = '0.0.0';
